b38bd7115af42482bbad35889dd41338f84c9f8d
jdimawea <94670505+jdimawea@users.noreply.github.com>
2022-02-16 22:57:11 -0700

    Add files via upload

---
+++ fruityloops/src/test/java/com/jonathandimawea/fruityloops/FruityloopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathandimawea.fruityloops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruityloopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
